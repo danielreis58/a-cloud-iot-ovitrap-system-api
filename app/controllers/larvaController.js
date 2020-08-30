d00c32d06a0ac3bbc5905a44a30d78c7663189d1
Daniel Reis <danielpreis@uol.com.br>
2020-08-30 18:53:05 -0300

    Initial commit

---
+++ app/controllers/larvaController.js
@@ -0,0 +1,57 @@
+const s3 = require('../config/s3.config.js');
+const env = require('../config/s3.env.js');
+const moment = require('moment');
+
+exports.doUpload = (req, res) => {
+    const filename = moment().format("YYYY-MM-DD-HH:mm:ss");
+
+    const params = {
+        Bucket: env.Bucket + '/Larvas',
+        Key: filename,
+        Body: req.file.buffer
+    }
+
+    s3.upload(params, (err, data) => {
+        if (err) {
+            res.status(500).send("Error -> " + err);
+        }
+        res.send("File uploaded successfully! -> keyname = " + filename);
+    });
+}
+
+exports.doDownload = (req, res) => {
+    const params = {
+        Bucket: env.Bucket + '/Larvas',
+        Key: req.params.filename
+    }
+
+    res.setHeader('Content-Disposition', 'attachment');
+
+    s3.getObject(params)
+        .createReadStream()
+        .on('error', function (err) {
+            res.status(500).json({ error: "Error -> " + err });
+        }).pipe(res);
+}
+
+exports.listKeyNames = (req, res) => {
+    const params = {
+        Bucket: env.Bucket,
+        Prefix: 'Larvas/',
+        StartAfter: 'Larvas/'
+    }
+
+    var keys = [];
+    s3.listObjectsV2(params, (err, data) => {
+        if (err) {
+            console.log(err, err.stack); // an error occurred
+            res.send("error -> " + err);
+        } else {
+            var contents = data.Contents;
+            contents.forEach(function (content) {
+                keys.push(content.Key.replace('Larvas/', ''));
+            });
+            res.send(keys);
+        }
+    });
+}
