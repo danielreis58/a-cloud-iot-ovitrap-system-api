42c4cf874fd55a9db534d78b37f17a3f2936b23f
Daniel Reis <daniel.reis@tech4h.com.br>
2021-05-20 09:03:29 -0300

    use cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import './database/index.js'
+import cors from 'cors'
 import express from 'express'
 import routes from './routes/index.js'
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use((err, req, res, next) => {
   if (err instanceof SyntaxError) {
